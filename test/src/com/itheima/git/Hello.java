ea95f1c559e2e2bc328197237222dd440bc1d0ec
aaa <aaa@123.com>
2019-06-29 19:40:49 +0800

    第一次修改

---
+++ test/src/com/itheima/git/Hello.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("How are you ?");
+        System.out.println("How are you ?");
+        System.out.println("How are you ?");
+        System.out.println("How are you ?");
+        System.out.println("How are you ?");
+    }
+}
